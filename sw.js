f08d9cee76897f84f07777f0b6111b9d6c2a8df6
dasha9727972-stack <dasha9727972@gmail.com>
2026-08-02 23:04:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(ks => Promise.all(ks.map(k => caches.delete(k)))));
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))));
   self.clients.claim();
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+self.addEventListener('fetch',e=>{
+  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
 });
